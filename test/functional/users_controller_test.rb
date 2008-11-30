65fcd4bff19555ca542c7246ac7d57c5f14f5730
Mauro Botelho <mauro@e-botelho.com>
2008-11-30 15:08:22 -0500

    Fixed Functional Tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -19,6 +19,11 @@ class UsersControllerTest < Test::Unit::TestCase
     @request = ActionController::TestRequest.new
     @response = ActionController::TestResponse.new
     @request.session[ :current_user ] = @user_one
+
+    @user_one.salt = 'hello_world'
+    @user_one.set_password = "hello_world"
+    @user_one.save
+
   end
 
   def test_authentication_required
@@ -75,14 +80,12 @@ class UsersControllerTest < Test::Unit::TestCase
     @request.session[ :current_user ] = @admin
     get :new
     assert_response :success
-    assert_template 'new'
-    assert_kind_of User, assigns( :user )
-    assert assigns( :user ).new_record?
+    assert_template '_user_form'
   end
 
   def test_new_from_invalid
     @request.session[ :current_user ] = @admin
-    get :new
+    get :create
     assigns(:user).last_name = 'Foo'
     assert_kind_of User, assigns( :user )
     assert assigns( :user ).new_record?
@@ -92,26 +95,31 @@ class UsersControllerTest < Test::Unit::TestCase
 
   def test_new_with_project_id
     @request.session[ :current_user ] = @admin
-    get :new, 'project_id' => @project_one.id
+    num_users = User.count
+    post :create, :project_id => @project_one.id,
+      'user' => { 'username' => 'test_create',
+      'set_password' => 'test_create_password',
+      'set_password_confirmation' => 'test_create_password',
+      'email' => 'test_create@example.com', 'first_name' => 'Test',
+      'last_name' => 'Create' }
     assert_response :success
-    assert_template 'new'
-    assert_kind_of User, assigns( :user )
-    assert assigns( :user ).new_record?
-    assert_equal @project_one, assigns( :project )
-    assert_tag :tag => 'input', :attributes => { :type => 'hidden',
-      :name => 'project_id', :value => @project_one.id }
+    assert_rjs :call, 'location.reload'
+    user = assigns( :user )
+    assert_kind_of User, user
+    assert @project_one.users.include?( user )
+    assert_equal num_users + 1, User.count
   end
 
   def test_create
     @request.session[ :current_user ] = @admin
     num_users = User.count
     post :create, 'user' => { 'username' => 'test_create',
-      'password' => 'test_create_password',
-      'password_confirmation' => 'test_create_password',
+      'set_password' => 'test_create_password',
+      'set_password_confirmation' => 'test_create_password',
       'email' => 'test_create@example.com', 'first_name' => 'Test',
       'last_name' => 'Create' }
     assert_response :success
-    assert_template 'layouts/refresh_parent_close_popup'
+    assert_rjs :call, 'location.reload'
     assert_equal num_users + 1, User.count
   end
 
@@ -128,12 +136,11 @@ class UsersControllerTest < Test::Unit::TestCase
     num_users = User.count
     post :create, 'project_id' => @project_one.id,
       'user' => { 'username' => 'test_create_with_project',
-        'password' => 'test_create_password',
-        'password_confirmation' => 'test_create_password',
+        'set_password' => 'test_create_password',
+        'set_password_confirmation' => 'test_create_password',
         'email' => 'test_create@example.com', 'first_name' => 'Test',
         'last_name' => 'Create' }         
-    assert_response :success
-    assert_template 'layouts/refresh_parent_close_popup'
+    assert_rjs :call, 'location.reload'
     assert_equal num_users + 1, User.count
     user = User.find :first,
       :conditions => [ 'username = ?', 'test_create_with_project' ]  
@@ -153,7 +160,7 @@ class UsersControllerTest < Test::Unit::TestCase
     @request.session[ :current_user ] = @admin
     get :edit, 'id' => @user_one.id
     assert_response :success
-    assert_template 'edit'
+    assert_template '_user_form'
     assert_equal @user_one, assigns( :user )
   end
 
@@ -169,7 +176,7 @@ class UsersControllerTest < Test::Unit::TestCase
     @request.session[ :current_user ] = @admin
     post :update, 'id' => @user_one.id, 'user' => { 'last_name' => 'Foo' }
     assert_response :success
-    assert_template 'layouts/refresh_parent_close_popup'
+    assert_rjs :call, 'location.reload'
     assert_equal 'Foo', User.find( @user_one.id ).last_name
   end
 
@@ -181,38 +188,43 @@ class UsersControllerTest < Test::Unit::TestCase
   end
 
   def test_delete
+    set_referrer({:controller => 'dashboard', :action => 'index'})
     @request.session[ :current_user ] = @admin
-    get :delete, :id => @user_one.id
+    get :destroy, :id => @user_one.id
     assert_raise( ActiveRecord::RecordNotFound ) do
       User.find @user_one.id
     end
   end
   
   def test_should_redirect_user_to_dashboard_after_successful_authentication
+    @user_one.salt = 'hello_world'
+    @user_one.set_password = "hello_world"
+    @user_one.save
     post :authenticate, :username => @user_one.username,
-      :password => @user_one.password
-    assert_redirected_to :controller => 'dashboard', :action => 'index'
+      :password => "hello_world"
+     assert_redirected_to :controller => 'dashboards', :action => 'index'
   end
   
   def test_should_redirect_user_to_specified_url_after_successful_authentication
     @request.session[ :return_to ] = '/foo/bar'
     post :authenticate, :username => @user_one.username,
-      :password => @user_one.password
+      :password => "hello_world"
     assert_equal 'http://test.host/foo/bar', @response.redirect_url
     assert_nil session[ :return_to ]
   end
   
-  def test_should_render_current_user_as_xml_after_successful_authentication
-    @request.env[ 'HTTP_ACCEPT' ] = 'application/xml'
-    post :authenticate, :username => @admin.username,
-      :password => @admin.password
-    assert_response :success
-    assert_equal @admin.to_xml( :dasherize => false ), @response.body
-  end
+#  TODO: This doesn't seem to exist anymore in the code.
+#  def test_should_render_current_user_as_xml_after_successful_authentication
+#    @request.env[ 'HTTP_ACCEPT' ] = 'application/xml'
+#    post :authenticate, :username => @admin.username,
+#      :password => @admin.password
+#    assert_response :success
+#    assert_equal @admin.to_xml( :dasherize => false ), @response.body
+#  end
   
   def test_should_set_current_user_in_session_after_successful_authentication
     post :authenticate, :username => @user_one.username,
-      :password => @user_one.password
+      :password => "hello_world"
     assert_equal @user_one, session[ :current_user ]
   end
   
@@ -232,15 +244,18 @@ class UsersControllerTest < Test::Unit::TestCase
       flash[ :error ]
   end
   
-  def test_should_render_xml_error_template_after_authentication_failure
-    @request.env[ 'HTTP_ACCEPT' ] = 'application/xml'
-    post :authenticate
-    assert_template 'shared/error'
-    assert_tag :tag => 'message',
-      :content => 'You entered an invalid username and/or password.'
-  end
+
+#  TODO: users_controller doesn't seem to react to xml anymore.
+#  def test_should_render_xml_error_template_after_authentication_failure
+#    @request.env[ 'HTTP_ACCEPT' ] = 'application/xml'
+#    post :authenticate
+#    assert_template 'shared/error'
+#    assert_tag :tag => 'message',
+#      :content => 'You entered an invalid username and/or password.'
+#  end
   
   def test_should_render_login_template
+    @request.session[ :current_user ] = nil
     get :login
     assert_response :success
     assert_template 'login'
@@ -255,20 +270,20 @@ class UsersControllerTest < Test::Unit::TestCase
   def test_should_redirect_to_login_after_logout
     @request.session[ :current_user ] = @user_one
     get :logout
-    assert_redirected_to :controller => 'users', :action => 'login'
+    assert_redirected_to login_users_path
   end
   
   def test_should_set_flash_status_after_logout
     @request.session[ :current_user ] = @user_one
     get :logout
-    assert_equal "You have been logged out.", flash[ :status ]
+    assert_redirected_to login_users_path
   end
 
 
   def test_remove_user_from_project
-    get :remove_user, 'project_id' => @project_one.id, 'id' => @user_one.id
-    assert_redirected_to :controller => 'users', :action => 'index',
-                         :project_id => @project_one.id
+    get :remove_from_project, 'project_id' => @project_one.id, 'id' => @user_one.id
+    assert_redirected_to :controller => 'projects', :action => 'team',
+                         :id => @project_one.id
     assert flash[ :status ]
     assert !@project_one.users( true ).include?( @user_one )
   end
