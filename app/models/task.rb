28e985c568fc83c74cf351b41c275a3e1c7396fd
Mauro Botelho <mabotelh@gmail.com>
2009-01-27 22:33:26 -0500

    changed the database and model to have status instead of completed

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,22 @@ class Task < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User', :foreign_key => :user_id
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :story_id
+  Statuses = []
+
+  composed_of :status, :mapping => %w(status order), :class_name => 'Story::Status'
+
+  class Status < Story::RankedValue
+    class << self
+      def new(order)
+        super(order, Statuses)
+      end
+    end
+
+    Statuses << New = create(1, 'New')
+    Statuses << InProgress = create(2, 'In Progress')
+    Statuses << ToVerify = create(3, 'To Verify')
+    Statuses << Complete = create(4, 'Complete')
+  end
 
   def assign_to(new_owner)
     self.owner = new_owner
